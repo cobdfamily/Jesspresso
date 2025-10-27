0fb86c0644a0424d220b77f6f207ad9a0b60d4ab
Alexandro Jurgensen <alexandrojurgensen@Chocolate-Lab.local>
2025-10-27 03:51:17 -0700

    Initial JesspressoOS bootstrap script created

---
+++ OS/bootstrap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# A script to bootstrap JesspressoOS
+# Copyright 2025 by Alexandro Jurgensen for Canadian Organization of the Blind and DeafBlind (COBD)
+# All rights reserved
+
+# “For Essence, who's love of coffee inspired the first step of this project. I simply wasn't okay with a coffee maker in my house you could not use.” Alexandro Jurgensen
+
+
